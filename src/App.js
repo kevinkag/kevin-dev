782e760ec06c67d0bea6970ae42be0ac6f7d7ab0
Kevin Martinez <keving28km@gmail.com>
2022-06-23 16:21:46 -0400

    deploy page 2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/pages/Home/Home';
 
 function App() {
+  //nada
   return (
       <React.Fragment>
         <BrowserRouter>
